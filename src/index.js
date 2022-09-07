da2dd7b7896ee6a87300a73d70325982768490d3
Francis <gwadafrank@gmail.com>
2022-09-07 23:16:33 +0300

    display characters on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Your code here
 const baseUrl = "http://localhost:3000/characters";
 const characters = document.querySelector('#character-bar')
+const characterInfo = document.querySelector('#detailed-info')
+
+
 const init = () =>{
     console.log('we are linve');
     // handleCharacterCard();
@@ -20,6 +23,15 @@ function handleCharacterCard(data){
     data.forEach(user => {
         let span =  document.createElement('span')
         span.textContent = user.name
+
+        span.addEventListener('click', ()=>{
+            console.log('click', user);
+
+            characterInfo.querySelector('#name').textContent = user.name
+            characterInfo.querySelector('#image').src = user.image
+            characterInfo.querySelector("#vote-count").textContent = user.votes;
+
+        })
         characters.appendChild(span)
         
 
