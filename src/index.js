ea73ca729b449d1468b155c4c7feec242f0187d6
Francis <gwadafrank@gmail.com>
2022-09-07 23:05:36 +0300

    retrieve characters and add them to dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // Your code here
+const baseUrl = "http://localhost:3000/characters";
+const characters = document.querySelector('#character-bar')
+const init = () =>{
+    console.log('we are linve');
+    // handleCharacterCard();
+    fetchData()
+
+}
+
+async function fetchData(){
+    const response = await fetch(baseUrl)
+    const data = await response.json()
+
+    console.log(data);
+    handleCharacterCard(data)
+}
+
+function handleCharacterCard(data){
+    data.forEach(user => {
+        let span =  document.createElement('span')
+        span.textContent = user.name
+        characters.appendChild(span)
+        
+
+    });
+
+
+
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
